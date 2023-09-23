521f45593841c29e3f35b61ccf1b91d168dfb558
elijaholmos <eli.olmos9@gmail.com>
2023-09-23 12:28:46 -0700

    update submission to perform watcher tasks

--- task/submission.js
+++ task/submission.js
@@ -1,39 +1,88 @@
 const { namespaceWrapper } = require('../_koiiNode/koiiNode');
+const { TickerWatcher, dynamicImport } = require('@metex/trading-alerts');
+const { WebClient } = require('@slack/web-api');
+const puppeteer = require('puppeteer');
+
 class Submission {
-  async task(round) {
-    // Write the logic to do the work required for submitting the values and optionally store the result in levelDB
+  task(round) {
+    // how to use stores and track multiple stocks at once?
 
-    // Below is just a sample of work that a task can do
+    return new Promise(async (resolve) => {
+      const value = 'Hello World!';
 
-    try {
-      const value = 'Hello, World!';
-      console.log('ROUND', round);
+      try {
+        const slack = new WebClient(process.env.SLACK_TOKEN);
+        console.log(`launching browser...`);
+        const browser = await puppeteer.launch({ headless: 'new' });
 
-      if (value) {
-        // store value on NeDB
-        await namespaceWrapper.storeSet('value', value);
+        const TICKERS = ['AAPL', 'GME', 'MSFT', 'TWTR', 'TSLA', 'AMZN'];
+
+        const priceChangeHandler = async ({ ticker, initialPrice, price, delta, threshold }) => {
+          console.log('price change!', ticker, initialPrice, price, delta);
+
+          console.log(`[${ticker}]: creating page...`);
+          const page = await browser.newPage();
+          console.log(`[${ticker}]: getting articles...`);
+          const articles = await (
+            await dynamicImport(
+              'https://raw.githubusercontent.com/elijaholmos/trading-alerts/main/scrapers/bloomberg.js',
+            )
+          ).run({ page, ticker });
+          console.log(`[${ticker}]: got articles!`, articles);
+          page.close();
+
+          let text = `[${ticker}]: Price change from ${initialPrice} to ${price} exceeds threshold (${
+            threshold * 100
+          }%): ${(delta * 100).toFixed(2)}%`;
+
+          if (!!articles?.length)
+            text += `\n\n*Possibly related articles*
+              \r${articles.map(({ title, url }) => `• <${url}|${title}>`).join('\n')}
+            `;
+
+          await slack.chat.postMessage({
+            channel: '#trading-alerts',
+            text,
+          });
+        };
+
+        const watchers = TICKERS.map(
+          (ticker) =>
+            new TickerWatcher({
+              ticker,
+              threshold: 0.0003,
+            }),
+        );
+        watchers.forEach((watcher) => watcher.on('priceChange', priceChangeHandler));
+
+        watchers.forEach((watcher) => watcher.on('open', () => console.log('watcher opened')));
+
+        watchers.forEach((watcher) =>
+          watcher.on('close', () => {
+            console.log('watcher closed');
+            resolve('watcher closed');
+          }),
+        );
+
+        if (value) {
+          // store value on NeDB
+          await namespaceWrapper.storeSet('value', value);
+        }
+      } catch (err) {
+        console.log('ERROR IN EXECUTING TASK', err);
+        resolve('ERROR IN EXECUTING TASK' + err);
       }
-      return value;
-    } catch (err) {
-      console.log('ERROR IN EXECUTING TASK', err);
-      return 'ERROR IN EXECUTING TASK' + err;
-    }
+    });
   }
 
   async submitTask(roundNumber) {
     console.log('submitTask called with round', roundNumber);
     try {
       console.log('inside try');
-      console.log(
-        await namespaceWrapper.getSlot(),
-        'current slot while calling submit',
-      );
+      console.log(await namespaceWrapper.getSlot(), 'current slot while calling submit');
       const submission = await this.fetchSubmission(roundNumber);
       console.log('SUBMISSION', submission);
-      await namespaceWrapper.checkSubmissionAndUpdateRound(
-        submission,
-        roundNumber,
-      );
+      await namespaceWrapper.checkSubmissionAndUpdateRound(submission, roundNumber);
       console.log('after the submission call');
       return submission;
     } catch (error) {
@@ -55,5 +104,6 @@ class Submission {
     return value;
   }
 }
+
 const submission = new Submission();
 module.exports = { submission };
